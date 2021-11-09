cf6a374984123d82615209cb9cb1ec38376916d1
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-11-09 19:06:09 +0100

    Creating models

--- CVaplication/models.py
+++ CVaplication/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    picture = models.FileField(blank=True, null=True)
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=100, null=True)
+    street = models.CharField(max_length=255, null=True)
+    title = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    link = models.TextField()
+
+
+class Experience(models.Model):
+    company = models.CharField(max_length=255)
+    function = models.CharField(max_length=255)
+    description = models.TextField()
+    started = models.DateField()
+    ended = models.DateField(null=True)
+
+
+class Education(models.Model):
+    school = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Skills(models.Model):
+    skill = models.TextField()
--- CvMaker/settings.py
+++ CvMaker/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'CVaplication.apps.CvaplicationConfig',
+    'djrichtextfield',
 ]
 
 MIDDLEWARE = [
