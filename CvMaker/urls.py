ee607e952d5f5224ac2d5907e791d76ff679912f
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-11-22 21:53:24 +0100

    Photos chaning and photos in urls paths

--- CvMaker/urls.py
+++ CvMaker/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django.contrib import admin
 from django.urls import path
-from CVaplication.views import MainPageView, ProfileAddPage
+from CVaplication.views import MainPageView , ResumeCreator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainPageView.as_view(), name='main_page'),
-    path('profile/', ProfileAddPage.as_view(), name='profile_add')
+    path('create/', ResumeCreator.as_view(), name='cv_maker')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
