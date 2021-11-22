c0b0841444ce6323955203be4fd95db9f2fb3869
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-11-22 21:52:12 +0100

    Resume creation view

--- CVaplication/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from CVaplication.models import Profile, Experience, Education, Skills
-
-
-class ProfileModelForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = '__all__'
-
--- CVaplication/views.py
+++ CVaplication/views.py
@@ -1,13 +1,6 @@
 from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
-from django.views.generic import CreateView
-
-from CVaplication import forms
-from CVaplication.models import Profile
-
-
 class MainPageView(View):
     # Main Page view
     def get(self, request):
@@ -15,9 +8,7 @@ class MainPageView(View):
         return response
 
 
-class ProfileAddPage(CreateView):
-    # Adding data to profile table
-    model = Profile
-    form_class = forms.ProfileModelForm
-    template_name = 'profile_add.html'
-    success_url = "/"
+class ResumeCreator(View):
+    def get(self, request):
+        response = render(request, 'profile_add.html')
+        return response
