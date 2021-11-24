05096f4a8aab9aaf48dd2118a28fff6af828e28e
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-11-24 18:59:43 +0100

    Form step 1 onkeyup

--- static/js/main.js
+++ static/js/main.js
@@ -3,11 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const button_next = document.querySelectorAll("#form_step_next")
     const button_previous = document.querySelectorAll("#form_step_back")
 
-    console.log(form_elements)
-    console.log(button_next)
-    console.log(button_previous)
-
-
     let counter = 0;
     button_next.forEach(function (button) {
         button.addEventListener('click', function () {
@@ -27,6 +22,56 @@ document.addEventListener("DOMContentLoaded", function () {
 
         })
     })
+    const first_name = document.querySelector("#first_name")
+    const first_name_input = document.getElementsByName('name')[0]
+    first_name_input.onkeyup = function () {
+        first_name.innerText = first_name_input.value
+    }
+
+    const last_name = document.querySelector("#last_name")
+    const last_name_input = document.getElementsByName("last_name")[0]
+    last_name_input.onkeyup = function () {
+        last_name.innerText = last_name_input.value
+    }
 
 
+    const city = document.querySelector("#city")
+    const city_input = document.getElementsByName("city")[0]
+    city_input.onkeyup = function () {
+        city.innerText = city_input.value
+    }
+
+
+    const street = document.querySelector("#street")
+    const street_input = document.getElementsByName("street")[0]
+    street_input.onkeyup = function () {
+        street.innerText = street_input.value
+    }
+
+
+    const position = document.querySelector("#position")
+    const position_input = document.getElementsByName("position")[0]
+    position_input.onkeyup = function () {
+        position.innerText = position_input.value
+    }
+
+
+    const email = document.querySelector("#email")
+    const email_input = document.getElementsByName("email")[0]
+    email_input.onkeyup = function () {
+        email.innerText = email_input.value
+    }
+
+
+    const phone_number = document.querySelector("#phone_number")
+    const phone_number_input = document.getElementsByName("phone_number")[0]
+    phone_number_input.onkeyup = function () {
+        phone_number.innerText = phone_number_input.value
+    }
+
+    const school = document.querySelector("#school")
+    const school_input = document.getElementsByName("school")[0]
+    school_input.onkeyup = function () {
+        school.innerText = school_input.value
+    }
 })
