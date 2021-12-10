c4fdf08649b394b907c6ccb3343c726d4a33b0ca
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-12-10 18:47:13 +0100

    Function that changes HTML file to pdf

---
+++ CVaplication/render_pdf.py
@@ -0,0 +1,22 @@
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+
+from xhtml2pdf import pisa
+
+
+def render_to_pdf(template_src, context_dict={}):
+    """
+    Function that takes a html file and returns it as a pdf.
+
+    :param template_src: Name of the template
+    :param context_dict:  Dictionary with data
+    :return: Pdf file from HTML, with error None
+    """
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("utf-8")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
